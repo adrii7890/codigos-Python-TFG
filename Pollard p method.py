a59d17a63d05231d06001f5e007aced645ba7f52
adrii7890 <99539625+adrii7890@users.noreply.github.com>
2022-02-12 14:10:31 +0100

    Update Pollard p method.py

--- Pollard p method.py
+++ Pollard p method.py
@@ -1,20 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import math
-
-
-# In[3]:
-
-
-import time
-
-
-# In[28]:
-
 
 def Pollardp (n,x0,M):
     a=x0
@@ -25,27 +9,9 @@ def Pollardp (n,x0,M):
         a=(a**2-1)%n
         b=(b**2-1)%n
         b=(b**2-1)%n
-        print(a ,'y', b)
         d=math.gcd(a-b,n)
         i+=1
     if d!=1 and d!=n:
         return [d,n/d]
     else:
         return 0
-
-
-# In[29]:
-
-
-inicio=time.time()
-m=Pollardp(12247,2,7)
-print(m)
-fin=time.time()
-print(fin-inicio)
-
-
-# In[ ]:
-
-
-
-
