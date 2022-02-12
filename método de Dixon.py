efee6e3a84bd30f162af81f4b09a7f6ecd1a8e64
adrii7890 <99539625+adrii7890@users.noreply.github.com>
2022-02-12 17:40:29 +0100

    Update and rename Dixon method.py to método de Dixon.py

---
+++ método de Dixon.py
@@ -0,0 +1,96 @@
+import math
+import numpy as np
+import random
+
+def  trialdiv(n):
+    L=[]
+    m=math.ceil(math.sqrt(n))
+    for i in  range (2,m+1):
+        while n%i==0  and n!=1:
+            L.append(i)
+            n=n/i
+    if n==1:
+        return L
+    else:
+        L.append(int(n))
+        return L 
+    
+def isprime(n): #decide si un número es primo
+    prime=True
+    i=2
+    while i<=math.sqrt(n) and prime==True:
+        if n%i==0:
+            prime=False
+        i+=1
+    return prime
+    
+def basef (M): #genera la base de factores 
+    B=[2]
+    i=3
+    while i<=M:
+        if isprime(i)==True:
+            B.append(i)
+        i+=1
+    return B
+
+def Bsmooth(n,B): #función que devuelve si un número es B-liso
+    smooth=True
+    L=trialdiv(n)
+    i=0
+    while smooth==True and i<len(L):
+        if L[i]>B:
+            smooth=False
+        i+=1
+    return smooth
+
+
+def exp(n,B): #función que devuelve el exponente módulo dos de un número con respecto a una cierta base de factores
+    L=trialdiv(n)
+    E = []
+    i=0
+    while len(L)>0 and i<len(B):
+        a=L[0]
+        if a==B[i]:
+            j=0
+            count=0
+            while j<len(L):
+                if L[j]==a:
+                    count+=1
+                    L.pop(j)
+                    j=0
+                else:
+                    j+=1
+            E.append(count%2)
+            i+=1
+        else:
+            i+=1
+            E.append(0)
+    while len(E)<len(B):
+        E.append(0)
+    return E        
+
+
+def Dixon(n,N,M): #primera parte del método de Dixon, la "recolección de datos". 
+    z=0                         #Se piden como entrada el número a factorizar, el número de elementos aleatorios con los que trabajaremos y la cota para la base de factores.
+    w=0
+    # Base de factores 
+    base = basef(M)
+    L=[]
+    #Generación de la lista aleatoria
+    while len(L)<N:
+        L.append(random.randrange(n)) #aleatorios en el intervalo [1,n]
+
+    B = []
+    Z = []
+    W=[]
+    i=0
+    #Búsqueda de números lisos para nuestra base de factores
+    while i<N:
+        z=L[i]
+        w=z**2%n 
+        if Bsmooth(w,M)==True:
+            Z.append(z)
+            W.append(w)
+            B.append(exp(w,base))
+        i+=1
+#tendríamos almacenada en B la matriz con los exponentes
