fa07a6b8e4df751ede0bd2530e02275d629854eb
adrii7890 <99539625+adrii7890@users.noreply.github.com>
2022-03-04 15:59:21 +0100

    Update Test de Lucas.py

--- Test de Lucas.py
+++ Test de Lucas.py
@@ -1,15 +1,3 @@
-
-def exprapID(base, num, mod): #algoritmo de la exponenciación rápida (variante izquierda a derecha)
-    acu=1
-    binary=format(num,"b")
-    i=0
-    while i<len(binary):
-        acu=(acu**2)%mod
-        if binary[i]=="1":
-            acu=(acu*base)%mod
-        i+=1
-    return acu
-
 def TestLucas(n,L): #L es una lista con los divisores de n-1
     a=2
     i=0
