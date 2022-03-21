230055099a2e91a912da19ff0cbd3b2a7c4bb7da
adrii7890 <99539625+adrii7890@users.noreply.github.com>
2022-03-21 16:27:21 +0100

    Update Prueba Fermat.py

--- Prueba Fermat.py
+++ Prueba Fermat.py
@@ -4,6 +4,6 @@ def pruebaFermat(n,k):
     for j in range(0,k):
         a=random.randint(2,n-1) #genera un entero aleatorio entre 2 y n-1, ambos incluidos
         print(a)
-        if exprapID(a,n-1,n)!=1: #vemos si pasa el test usando el algoritmo de la exponenciación rápida, cuyo código se puede ver también en el repositorio
+        if binladders(a,n-1,n)!=1: #vemos si pasa el test usando el algoritmo de la exponenciación rápida, cuyo código se puede ver también en el repositorio
             return False
     return True
