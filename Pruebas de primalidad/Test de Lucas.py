1d6fd1178a8e762920baf16976eb027ce2e53b53
adrii7890 <99539625+adrii7890@users.noreply.github.com>
2022-04-03 13:26:02 +0200

    Update Test de Lucas.py

--- Pruebas de primalidad/Test de Lucas.py
+++ Pruebas de primalidad/Test de Lucas.py
@@ -6,11 +6,11 @@ def TestLucas(n,L): #L es una lista con los divisores de n-1
         if binladders(2, n-1,n)!=1:     #usamos el algoritmo de la exponenciación binaria, cuyo código se puede ver también en este repositorio
             return False
         else:
-            while i<len(L) and bool=True:
+            while i<len(L) and bool==True:
                 if binladders(2,(n-1)//L[i],n)==1: 
                     bool=False
                 i+=1
-        if bool==True:
+        if bool==True:      #si todos son dsitintos de 1, entonces n es primo
             return True
         a+=1
     return False
