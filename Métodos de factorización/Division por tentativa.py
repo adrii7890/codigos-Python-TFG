cb9cc31ee7b9cf1a6c587b91829170acaffb5b4a
adrii7890 <99539625+adrii7890@users.noreply.github.com>
2022-04-03 13:30:18 +0200

    Update Division por tentativa.py

--- Métodos de factorización/Division por tentativa.py
+++ Métodos de factorización/Division por tentativa.py
@@ -4,7 +4,7 @@ def trialdiv(n):
     for i in range (2,m):
         while n%i==0 and n!=1:
             L.append(i)
-            n=n/i
+            n=n//i
             m=NRsuelo(n)
     if n==1:
         return L
