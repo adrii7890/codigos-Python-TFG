f214bb727e5beabf9193bf1ab706b6a4bd95a7f7
adrii7890 <99539625+adrii7890@users.noreply.github.com>
2022-04-03 13:39:45 +0200

    Update Método de Fermat.py

--- Métodos de factorización/Método de Fermat.py
+++ Métodos de factorización/Método de Fermat.py
@@ -1,27 +1,17 @@
-import math
-
-
-def NRtecho(n):  #Calcula la función techo de la raiz cuadrada usando el método de Newton
-    x = n
-    y = math.ceil((x + 1) / 2)
-    while y < x:
-        x = y
-        y = math.ceil((x + n / x) / 2)
-    return x
-
+from math import sqrt
 
 def isPerfectSquare(num): #determina si num es un cuadrado perfecto
-        return math.sqrt(num) % 1 == 0
+        return NRsuelo(num)**2 == num
 
 
 def FermatFact1(n,M):
-    x=NRtecho(n)
+    x=NRsuelo(n)+1      #función techo de
     j=1
     while j<=M and isPerfectSquare(x**2-n)==False:
         x+=1
         j+=1
     if isPerfectSquare(x**2-n)==True:
-        y=math.sqrt(x**2-n)
+        y=sqrt(x**2-n)
         return [(x-y),(y+x)]
     else:
         return 0 #Devuelve 0 si el método falla
