0cf5a8ec12d5e5d2a1e0783bf54a9112b3c2c59f
adrii7890 <99539625+adrii7890@users.noreply.github.com>
2022-04-13 13:05:12 +0200

    Delete Algoritmos complementarios directory

--- Algoritmos complementarios/Cuadrados iterados.py
+++
@@ -1,9 +0,0 @@
-def binladders(base, num, mod):
-    acu=base
-    binary=format(num,"b")
-    i=1
-    while i<len(binary):
-        acu=(acu**2)%mod
-        if binary[i]=="1":
-            acu=(acu*base)%mod
-        i+=1
--- Algoritmos complementarios/Método de Newton para la parte entera de una raíz cuadrada.py
+++
@@ -1,9 +0,0 @@
-from math import floor
-
-def NRsuelo(n): 
-    x = n
-    y = floor((x + 1) / 2)
-    while y < x:
-        x = y
-        y = floor((x + n / x) / 2)
-    return x
