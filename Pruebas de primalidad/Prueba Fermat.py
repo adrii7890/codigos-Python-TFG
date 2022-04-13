0dc2964eac3fc6ca3d0b80461272055139c74baf
adrii7890 <99539625+adrii7890@users.noreply.github.com>
2022-04-13 13:12:08 +0200

    Update Prueba Fermat.py

--- Pruebas de primalidad/Prueba Fermat.py
+++ Pruebas de primalidad/Prueba Fermat.py
@@ -1,9 +1,10 @@
 from random import randint
+import algoritmos_complementarios as alg
 
 def pruebaFermat(n,k):
     for j in range(0,k):
         a=randint(2,n-1) #genera un entero aleatorio entre 2 y n-1, ambos incluidos
         print(a)
-        if binladders(a,n-1,n)!=1: #vemos si pasa el test usando el algoritmo de la exponenciación rápida, cuyo código se puede ver también en el repositorio
+        if alg.binladders(a,n-1,n)!=1: #vemos si pasa el test usando el algoritmo de la exponenciación rápida, cuyo código se puede ver también en el repositorio
             return False
     return True
