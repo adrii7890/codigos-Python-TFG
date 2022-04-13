58af2d3ac34435836ac8404c8cddad366317f4be
adrii7890 <99539625+adrii7890@users.noreply.github.com>
2022-04-13 13:14:54 +0200

    Update Método de Fermat.py

--- Métodos de factorización/Método de Fermat.py
+++ Métodos de factorización/Método de Fermat.py
@@ -1,17 +1,17 @@
-from math import sqrt
+import algoritmos_complementarios as alg
 
 def isPerfectSquare(num): #determina si num es un cuadrado perfecto
-        return NRsuelo(num)**2 == num
+        return alg.NRsuelo(num)**2 == num
 
 
 def FermatFact1(n,M):
-    x=NRsuelo(n)+1 
+    x=alg.NRsuelo(n)+1 
     j=1
     while j<=M and isPerfectSquare(x**2-n)==False:
         x+=1
         j+=1
     if isPerfectSquare(x**2-n)==True:
-        y=sqrt(x**2-n)
+        y=alg.NRsuelo(x**2-n)
         return [(x-y),(y+x)]
     else:
         return 0 #Devuelve 0 si el método falla
