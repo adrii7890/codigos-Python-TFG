37e49c148a0f6eabec031d3fe4e2606e2c145ad9
adrii7890 <99539625+adrii7890@users.noreply.github.com>
2022-04-13 13:10:29 +0200

    Update Solovay-Strassen.py

--- Pruebas de primalidad/Solovay-Strassen.py
+++ Pruebas de primalidad/Solovay-Strassen.py
@@ -1,10 +1,12 @@
+import algoritmos_complementarios as alg
+
 def PruebSS(n,k):
     for j in range(1,k):
         a=random.randint(2,n-1)
-        x=jacobi(a,n)
+        x=alg.jacobi(a,n)
         print(a,x)
         if x==-1:
             x=n-1
-        if (x!=binladders(a,(n-1)//2,n)):
+        if (x!=alg.binladders(a,(n-1)//2,n)):
             return False
     return True
