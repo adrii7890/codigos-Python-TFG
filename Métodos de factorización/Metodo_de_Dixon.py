400db5d87f7bff26ca06b9276e3839cf5ae66478
adrii7890 <99539625+adrii7890@users.noreply.github.com>
2022-05-10 23:02:24 +0200

    Update Metodo_de_Dixon.py

--- Métodos de factorización/Metodo_de_Dixon.py
+++ Métodos de factorización/Metodo_de_Dixon.py
@@ -6,7 +6,7 @@ def  trialdiv(n):
     if n<0:
         n=math.fabs(n)
         L.append(-1)
-    m=math.ceil(math.sqrt(math.fabs(n)))
+    m=alg.NRfloor(n)
     for i in  range (2,m+1):
         while n%i==0  and n!=1:
             L.append(i)
