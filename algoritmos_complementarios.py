bbabd26234b4c8292d974dfb20bf30a99f88909b
adrii7890 <99539625+adrii7890@users.noreply.github.com>
2022-05-10 21:00:16 +0200

    Update algoritmos_complementarios.py

--- algoritmos_complementarios.py
+++ algoritmos_complementarios.py
@@ -72,3 +72,24 @@ def suclucas(n,r, P,Q): #calcula U_r(n) y V_(r) para P y Q dados
             U1aux=U1
         if k==r:
             return [U1,(2*U2-P*U1)%n]
+        
+def MR(n,k):
+    j, d= 0, n-1
+    while d%2==0:
+        j+=1
+        d//=2
+    for _ in range(0,k):
+        a=randint(2,n-1)
+        e=alg.binladders(a,d,n)
+        if e!=1 and e!=n-1:              #Comprobamos que no es -1, si lo es pasamos a otra base
+            i=1
+            bool=True
+            while i<j and bool==True:
+                e=e**2%n
+                if e==n-1:
+                    bool=False         #Si hay un -1 entonces pasa el test para esa base, seguimos con la siguiente
+                i+=1
+            if bool==True:              #Si no hay ningún -1 entonces n es compuesto
+                return False
+            
+    return True
