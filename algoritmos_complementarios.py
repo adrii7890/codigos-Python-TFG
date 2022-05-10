f6ad553af74ba19050d4b49460121d2d23170cc9
adrii7890 <99539625+adrii7890@users.noreply.github.com>
2022-05-10 22:45:51 +0200

    Update algoritmos_complementarios.py

--- algoritmos_complementarios.py
+++ algoritmos_complementarios.py
@@ -1,4 +1,29 @@
 from math import floor
+from random import randint
+
+def NRfloor(n): #Calculo de la función suelo de la raiz cuadrada mediante el método de Newton
+    x = n
+    y = math.floor((x + 1) / 2)
+    while y < x:
+        x = y
+        y = math.floor((x + n / x) / 2)
+    return x
+
+def NRsuelo(n): 
+    x = n
+    y = floor((x + 1) / 2)
+    while y < x:
+        x = y
+        y = floor((x + n / x) / 2)
+    return x
+
+def trialdiv(n): #modificado limite 1000 (para Baillie_PSW)
+    L=[]
+    m=NRsuelo(n)
+    for i in range (2,min(NRsuelo(n), 1000)):
+        if n%i==0:
+            return False
+    return True
 
 def binladders(base, num, mod):
     acu=base
@@ -10,14 +35,6 @@ def binladders(base, num, mod):
             acu=(acu*base)%mod
         i+=1
     return acu
-
-def NRsuelo(n): 
-    x = n
-    y = floor((x + 1) / 2)
-    while y < x:
-        x = y
-        y = floor((x + n / x) / 2)
-    return x
   
 def jacobi(a,n):
     sol=0
@@ -49,47 +66,49 @@ def jacobi(a,n):
  
     return sol
 
-def suclucas(n,r, P,Q): #calcula U_r(n) y V_(r) para P y Q dados
-
-    v1 = format(n,"b")
-    v2 = format(r,"b")
-    l1 = len(v1)
-    l2 = len(v2)
-    U1 = 1
-    U2 = P
-    U1aux=U1
-    k = 1
-    for i in range( 1, l2):
-        if v2[i] == '0':
-            k = 2*k;
-            U1 = (2*U1*U2 - P*U1**2)%n
-            U2 = (U2**2 - Q*U1aux**2)%n
-            U1aux=U1
-        else:
-            k = 2*k + 1;
-            U1 = (U2**2 - Q*U1**2)%n
-            U2 = (P*U2**2 - 2*Q*U1aux*U2)%n
-            U1aux=U1
-        if k==r:
-            return [U1,(2*U2-P*U1)%n]
-        
-def MR(n,k): #Miller-Rabin
+def suclucas(n,r, P,Q):
+    if r==1:
+        return [1,1]
+    else:
+        v1 = format(n,"b")
+        v2 = format(r,"b")
+        l1 = len(v1)
+        l2 = len(v2)
+        U1 = 1
+        U2 = P
+        U1aux=U1
+        k = 1
+        for i in range( 1, l2):
+            if v2[i] == '0':
+                k = 2*k;
+                U1 = (2*U1*U2 - P*U1**2)%n
+                U2 = (U2**2 - Q*U1aux**2)%n
+                U1aux=U1
+            else:
+                k = 2*k + 1;
+                U1 = (U2**2 - Q*U1**2)%n
+                U2 = (P*U2**2 - 2*Q*U1aux*U2)%n
+                U1aux=U1
+            if k==r:
+                return [U1,(2*U2-P*U1)%n]
+     
+def MR(n,k):
     j, d= 0, n-1
     while d%2==0:
         j+=1
         d//=2
     for _ in range(0,k):
         a=randint(2,n-1)
-        e=alg.binladders(a,d,n)
-        if e!=1 and e!=n-1:              
+        e=binladders(a,d,n)
+        if e!=1 and e!=n-1:            
             i=1
             bool=True
             while i<j and bool==True:
                 e=e**2%n
                 if e==n-1:
-                    bool=False         
+                    bool=False       
                 i+=1
-            if bool==True:              
+            if bool==True:   
                 return False
             
     return True
