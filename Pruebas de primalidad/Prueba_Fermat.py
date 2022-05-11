7e91a0b3aa0b83e79423cb2194f8f9a6872bfbde
adrii7890 <99539625+adrii7890@users.noreply.github.com>
2022-05-11 13:15:41 +0200

    Update Prueba_Fermat.py

--- Pruebas de primalidad/Prueba_Fermat.py
+++ Pruebas de primalidad/Prueba_Fermat.py
@@ -1,10 +1,9 @@
 from random import randint
 import algoritmos_complementarios as alg
 
-def pruebaFermat(n,k):
+def pruebaFermat(n,k): #número al que se le pasa el test y número de bases aleatorias para las que queremos aplicar el test
     for j in range(0,k):
         a=randint(2,n-1) #genera un entero aleatorio entre 2 y n-1, ambos incluidos
-        print(a)
         if alg.binladders(a,n-1,n)!=1: #vemos si pasa el test usando el algoritmo de cuadrados iterados, cuyo código se puede ver también en el repositorio
             return False
     return True
