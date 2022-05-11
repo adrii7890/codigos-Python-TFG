c97469da8a49443bf2c0a65f6ad59423367d6a14
adrii7890 <99539625+adrii7890@users.noreply.github.com>
2022-05-11 12:18:38 +0200

    Update Baillie_PSW.py

--- Pruebas de primalidad/Baillie_PSW.py
+++ Pruebas de primalidad/Baillie_PSW.py
@@ -1,12 +1,11 @@
 
 import algoritmos_complementarios as alg
-import Miller_Rabin as MR
 import Prueba_fuerte_de_Lucas as LucasS
 
 def baillie_PSW(n):
     if alg.trialdiv(n)==False:
         return False
-    elif MR.MR(n)==False:
+    elif alg.MR2(n)==False:
             return False
     elif LucasS.LucasStest(n)==False:
         return False
